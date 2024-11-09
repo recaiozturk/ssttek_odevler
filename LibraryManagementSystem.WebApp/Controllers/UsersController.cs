be284ac9d513d3b2f27dfc64509d199a857a06e8
recaiozturk <recaiozturk54@gmail.com>
2024-11-09 18:51:17 +0300

    created new branch for odev7

---
+++ LibraryManagementSystem.WebApp/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.WebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
